16beadf42a1bd74944630a631514c4cc6aba14ba
Mark Hintz <mark@interactivethings.com>
2014-12-15 17:53:51 +0100

    update

--- src/constellation.js
+++ src/constellation.js
@@ -1,12 +1,18 @@
-var songData;
+var songData
+  , mainSong
 
 window.preload = function() {
   songData = loadJSON("../data/out/songinfo-spotify.json")
 };
 
 window.setup = function() {
-  console.log(songData);
   createCanvas(800, 800)
+
+  mainSong = songData.filter(function(d) {
+    return !!d.title.toLowerCase().match(/along the watchtower/g)
+  })[0]
+
+  
 }
 
 window.draw = function() {
