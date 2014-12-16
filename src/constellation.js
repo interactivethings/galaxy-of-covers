ce69692768964898cb452892bec9fbbc1d12a718
Mark Hintz <markohintz@gmail.com>
2014-12-16 16:21:17 +0100

    Add the second song

--- src/constellation.js
+++ src/constellation.js
@@ -1,29 +1,69 @@
-var songData
+var allSongs
   , mainSong
   , mainSongCovers
   , dRange
   , gRange
   , gScale
+  , sXScale
+  , sYScale
+  , xSpace
+  , ySpace
 
 window.preload = function() {
-  songData = loadJSON("../data/out/whosampled.json")
+  allSongs = loadJSON("../data/out/whosampled.json")
 };
+
+function processData(songData) {
+  songData.forEach(function(d) {
+    d.date = new Date(+d.year, 0);
+  })
+  songData.sort(function(a, b) {
+    return a.date.getTime() - b.date.getTime()
+  })
+  return songData
+}
+
+function identity(_) { return _ }
+
+function unique(arr, fn) {
+  fn || (fn = identity)
+  var seen = {}
+    , result = []
+    , val
+  for (var i = 0, l = arr.length; i < l; ++i) {
+    val = fn(arr[i], i)
+    if (!seen[val]) {
+      seen[val] = true
+      result.push(val)
+    }
+  }
+  return result
+}
+
+function flatten(arr) {
+  return [].concat.apply([], arr)
+}
+
+function range(start, stop, step) {
+  var arr = []
+  for ( ; start < stop; start += step) {
+    arr.push(start)
+  }
+  return arr
+}
+
+function max(arr) {
+  var i = -1, n = arr.length, m, t
+  while (++i < n) if ((t = array[i]) != null && t > m) m = t;
+  return m
+}
 
 window.setup = function() {
   createCanvas(windowWidth, windowHeight)
 
-  /*mainSong = songData.filter(function(d) {
-    console.log(d);
-    return !!d.title.toLowerCase().match(/along the watchtower/g)
-  })[0]*/
+  allSongs = allSongs.map(processData)
 
-  mainSongCovers = songData[0];
-  mainSongCovers.forEach(function(d) {
-    d.date = new Date(+d.year, 0);
-  })
-  mainSongCovers.sort(function(a, b) {
-    return a.date.getTime() - b.date.getTime()
-  })
+  mainSongCovers = allSongs[0];
 
   dRange = mainSongCovers.reduce(function(m, v) {
     m[0] = Math.min(m[0], v.date.getTime())
@@ -31,51 +71,64 @@ window.setup = function() {
     return m
   }, [Infinity, -Infinity])
 
-  gRange = mainSongCovers.reduce(function(m, v) {
-    var g = v.genre
-    if (!~m.indexOf(g)) m.push(g)
-    return m
-  }, [])
-
-//  gRange.sort()
+  gRange = unique(flatten(allSongs.map(function(_) {
+    return unique(_, function(v) { return v.genre })
+  })))
 
   gScale = {}
   var interval = TWO_PI / gRange.length;
   for (var i = 0, a = 0, l = gRange.length; i < l; i++, a += interval) {
     gScale[gRange[i]] = a
   }
+
+  xSpace = width / allSongs.length
+  ySpace = height / allSongs.length
+
+  starGap = xSpace / (max(allSongs.map(function(arr) {
+    return arr.length
+  })) / 5)
+
+  sXScale = range(0, width, xSpace)
+  sYScale = range(0, height, ySpace)
 }
 
 window.draw = function() {
   background(16)
 
-  translate(width / 2, height / 2)
+  fill(255, 255, 255, 200)
+  stroke(255, 255, 255, 255)
+  strokeWeight(1.5)
 
-  fill(255)
-//  strokeWeight(0)
-  strokeWeight(2.5)
-  stroke(255)
-
-  drawStar(mainSongCovers, 0)
+  allSongs.forEach(function(covers, i) {
+    push()
+    translate(sXScale[i], sYScale[i])
+    translate(xSpace / 2, ySpace / 2)
+    drawStar(covers, 0)
+    pop()
+  })
 }
 
 function drawStar(list, index) {
-  var size = lerp(10, 30, +list[index].rank / list.length)
+  var size = starSize(+list[index].rank / list.length)
   ellipse(0, 0, size, size)
   if (!list[index + 1]) return true;
   var nextDatum = list[index + 1]
-  var nextPos = plotNextStar(nextDatum.genre, nextDatum.date.getTime())
+  var nextPos = nextStarPoint(nextDatum.genre, nextDatum.date.getTime())
   line(0, 0, nextPos.x, nextPos.y)
   translate(nextPos.x, nextPos.y)
   drawStar(list, index + 1)
+}
+
+function starSize(t) {
+  return lerp(2, 20, t)
 }
 
 function polarToEuclid(t, r) {
   return createVector(cos(t) * r, sin(t) * r)
 }
 
-function plotNextStar(genre, date) {
+function nextStarPoint(genre, date) {
   var t = gScale[genre]
-  var r = map(date, dRange[0], dRange[1], 0, 200)
+  var r = map(date, dRange[0], dRange[1], 4, starGap)
   return polarToEuclid(t, r)
 }
