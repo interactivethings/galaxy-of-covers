015a3658fb682274db70d314834af9cc74a72a89
Mark Hintz <mark@interactivethings.com>
2015-01-15 12:48:09 +0100

    App Header share open transition

--- src/components/AppHeader/AppHeader.jsx
+++ src/components/AppHeader/AppHeader.jsx
@@ -34,34 +34,37 @@ var AppHeader = React.createClass({
   render() {
     var dynamicState = this.props.dynamicState
 
-    console.log('render header');
-
     return (
       <div className="AppHeader" >
         <div className="AppHeader--back" onClick={this.navigateBack} >
           <h1 className="AppHeader--title" >A Galaxy of Covers</h1>
         </div>
-        <div className={"AppHeader--navigation " + (dynamicState.get('shareOpen') ? 'AppHeader--shareopen' : '')} >
-          <div className="AppHeader--navoption" onClick={this.toggleLegend} >
-            <h2 className="AppHeader--navlabel">{"Legend"}</h2>
-            <span className={"AppHeader-icon " + (dynamicState.get('legendOpen') ? "icon-keyboard-arrow-up" : "icon-keyboard-arrow-down")} />
+        {!dynamicState.get('shareOpen') ? (
+          <div className={"AppHeader--navigation"} >
+            <div className="AppHeader--navoption" onClick={this.toggleLegend} >
+              <h2 className="AppHeader--navlabel">{"Legend"}</h2>
+              <span className={"AppHeader-icon " + (dynamicState.get('legendOpen') ? "icon-keyboard-arrow-up" : "icon-keyboard-arrow-down")} />
+            </div>
+            <div className="AppHeader--navoption" onClick={this.toggleAbout} >
+              <h2 className="AppHeader--navlabel">{"About"}</h2>
+              <span className={"AppHeader-icon " + (dynamicState.get('aboutOpen') ? "icon-keyboard-arrow-up" : "icon-keyboard-arrow-down")} />
+            </div>
+            <div className="AppHeader--navoption" onClick={this.openShare} >
+              <h2 className="AppHeader--navlabel">{"Share"}</h2>
+              <span className="AppHeader-icon icon-add" />
+            </div>
           </div>
-          <div className="AppHeader--navoption" onClick={this.toggleAbout} >
-            <h2 className="AppHeader--navlabel">{"About"}</h2>
-            <span className={"AppHeader-icon " + (dynamicState.get('aboutOpen') ? "icon-keyboard-arrow-up" : "icon-keyboard-arrow-down")} />
+        ) : (
+          <div className={"AppHeader--navigation"} >
+            <div className="AppHeader--navoption" >
+              <h2 className="AppHeader--navlabel" onClick={this.closeShare} >{"Share"}</h2>
+              <span className="AppHeader-icon icon-close" onClick={this.closeShare} />
+              <div className="AppHeader-icon AppHeader-shareicon icon-twitter" />
+              <div className="AppHeader-icon AppHeader-shareicon icon-facebook" />
+              <div className="AppHeader-icon AppHeader-shareicon icon-pinterest" />
+            </div>
           </div>
-          <div className="AppHeader--navoption AppHeader-shareoptions" onClick={dynamicState.get('shareOpen') ? '' : this.openShare} >
-            <h2 className="AppHeader--navlabel">{"Share"}</h2>
-            {
-              dynamicState.get('shareOpen')
-              ? <span className="AppHeader-icon icon-close" onClick={this.closeShare} />
-              : <span className="AppHeader-icon icon-add" />
-            }
-            <div className="AppHeader-icon AppHeader-shareicon icon-twitter" />
-            <div className="AppHeader-icon AppHeader-shareicon icon-facebook" />
-            <div className="AppHeader-icon AppHeader-shareicon icon-pinterest" />
-          </div>
-        </div>
+        )}
       </div>
     )
   }
