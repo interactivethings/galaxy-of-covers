59cc6caa30e8f0fe4a7409deae4577f7da901395
Mark Hintz <mark@interactivethings.com>
2015-03-11 11:54:18 +0100

    Edit axis star

--- src/components/DetailView/AxisStar.js
+++ src/components/DetailView/AxisStar.js
@@ -1,16 +1,12 @@
-function AxisStar(selection, yPosition) {
-  var star = selection.selectAll('.SongDetailStar')
-    .data([0])
+var svgutil = require('util/svgutil')
 
-  if (star.empty()) {
-    star = selection.append('circle')
-      .attr('class', 'SongDetailStar')
-      .attr('opacity', 0)
-      .attr('cx', 0)
-      .attr('r', 8)
-  }
+function AxisStar(selection, yPosition) {
+  var star = svgutil.acquire(selection, 'SongDetailStar', 'circle')
 
   star
+    .attr('opacity', 0)
+    .attr('cx', 22)
+    .attr('r', 8)
     .attr('cy', yPosition)
     .transition()
     .attr('opacity', 1)
