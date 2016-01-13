1ed4df50b6ff6f1a71f454a272ac9049c26b5cef
Mark Hintz <mark@interactivethings.com>
2016-01-13 19:47:58 +0100

    Adjust Constants style

--- src/Constants.js
+++ src/Constants.js
@@ -1,8 +1,8 @@
-var Constants =
-{ DATA_URL: 'data/out/songinfo-production.json'
-, SYSTEM_RADIUS: 260
-, SYSTEM_BACKGROUND_RADIUS: 275
-, SYSTEM_PADDING: 30
-}
 
-module.exports = Constants
+
+module.exports = {
+  DATA_URL: 'data/out/songinfo-production.json',
+  SYSTEM_RADIUS: 260,
+  SYSTEM_BACKGROUND_RADIUS: 275,
+  SYSTEM_PADDING: 30
+};
