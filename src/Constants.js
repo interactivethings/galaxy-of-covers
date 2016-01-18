b96f584fea2a8d6296022879aef2253f48776902
Mark Hintz <mark@interactivethings.com>
2016-01-18 18:29:05 +0100

    Test system radius against window width

--- src/Constants.js
+++ src/Constants.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   DATA_URL: 'data/out/songinfo-production.json',
-  SYSTEM_RADIUS: 260,
+  SYSTEM_RADIUS: Math.min(260, window.innerWidth / 2),
   SYSTEM_BACKGROUND_RADIUS: 275,
   SYSTEM_PADDING: 20
 };
